39602a2208f271ab8d3751402d928cc4b5bf081d
Nikos Kougianos <kougi_jr@hotmail.com>
2019-11-21 19:31:50 +0200

    first commit

---
+++ src/views/index-sections/Download.js
@@ -0,0 +1,140 @@
+import React from "react";
+
+// reactstrap components
+import { Button, Container, Row, Col, UncontrolledTooltip } from "reactstrap";
+
+// core components
+
+function Download() {
+  return (
+    <>
+      <div
+        className="section section-download"
+        data-background-color="black"
+        id="download-section"
+      >
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col className="text-center" lg="8" md="12">
+              <h3 className="title">Do you love this Bootstrap 4 UI Kit?</h3>
+              <h5 className="description">
+                lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+              </h5>
+            </Col>
+            <Col className="text-center" lg="8" md="12">
+              <Button
+                className="btn-round mr-1"
+                color="info"
+                href=""
+                role="button"
+                size="lg"
+              >
+                Download React
+              </Button>
+              <Button
+                className="btn-round"
+                color="primary"
+                href=""
+                outline
+                role="button"
+                size="lg"
+                target="_blank"
+              >
+                Download PSD/Sketch
+              </Button>
+            </Col>
+          </Row>
+          <br></br>
+          <br></br>
+          <br></br>
+          <Row className="text-center mt-5">
+            <Col className="ml-auto mr-auto" md="8">
+              <h2>Want more?</h2>
+              <h5 className="description">
+                We're going to launch{" "}
+                . It will have huge number of components, sections and example
+                pages so you can start your development with a badass Bootstrap
+                4 UI Kit.
+              </h5>
+            </Col>
+            <Col md="12">
+              <Button
+                className="btn-neutral btn-round"
+                color="default"
+                href=""
+                size="lg"
+                disabled
+              >
+                <i className="now-ui-icons arrows-1_share-66 mr-1"></i>
+                Upgrade to PRO
+              </Button>
+            </Col>
+          </Row>
+          <br></br>
+          <br></br>
+          <Row className="justify-content-md-center sharing-area text-center">
+            <Col className="text-center" lg="8" md="12">
+              <h3>Thank you for supporting us!</h3>
+            </Col>
+            <Col className="text-center" lg="8" md="12">
+              <Button
+                className="btn-neutral btn-icon btn-round"
+                color="twitter"
+                href=""
+                id="tooltip86114138"
+                size="lg"
+                target="_blank"
+              >
+                <i className="fab fa-twitter"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip86114138">
+                Follow us
+              </UncontrolledTooltip>
+              <Button
+                className="btn-neutral btn-icon btn-round"
+                color="facebook"
+                href=""
+                id="tooltip735272548"
+                size="lg"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-square"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip735272548">
+                Like us
+              </UncontrolledTooltip>
+              <Button
+                className="btn-neutral btn-icon btn-round"
+                color="linkedin"
+                href=""
+                id="tooltip647117716"
+                size="lg"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip647117716">
+                Follow us
+              </UncontrolledTooltip>
+              <Button
+                className="btn-neutral btn-icon btn-round"
+                color="github"
+                href=""
+                id="tooltip331904895"
+                size="lg"
+                target="_blank"
+              >
+                <i className="fab fa-github"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip331904895">
+                Star on Github
+              </UncontrolledTooltip>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Download;
