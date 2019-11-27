4ab54e2934e69d8aceef01c5a2ee0ec4217c214b
Nikos Kougianos <kougi_jr@hotmail.com>
2019-11-27 16:22:14 +0200

    Updates

---
+++ precache-manifest.74a423212115521bf96d5dd7d7d29261.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26a965026bee1952041d34ed0655fa6",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "769f02d84f8067cf1090",
+    "url": "/react-demo/static/css/main.f1489d53.chunk.css"
+  },
+  {
+    "revision": "a056c88a7961dd752a4f",
+    "url": "/react-demo/static/js/2.bd27a77b.chunk.js"
+  },
+  {
+    "revision": "769f02d84f8067cf1090",
+    "url": "/react-demo/static/js/main.fb80e2bf.chunk.js"
+  },
+  {
+    "revision": "78404b4b0f010a1ef451",
+    "url": "/react-demo/static/js/runtime~main.3dcdfef1.js"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "/react-demo/static/media/bg4.4bff5d45.jpg"
+  },
+  {
+    "revision": "f1c18caac51c7c8bc99ffe87adb9a350",
+    "url": "/react-demo/static/media/kougianos.f1c18caa.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/react-demo/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/react-demo/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/react-demo/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/react-demo/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
